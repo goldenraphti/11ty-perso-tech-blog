392e6b5ce93306bb2cdc5e6238000883fb34191c
raphaelferrand <ferrand@protonmail.ch>
2020-10-21 20:19:57 +0200

    display conditionally nav links & footer links

---
+++ _data/editorSettings.js
@@ -0,0 +1,20 @@
+module.exports = {
+	translations: {
+		enabled: false,
+	},
+	darkMode: {
+		enabled: false,
+	},
+	email: {
+		enabled: true,
+	},
+	twitter: {
+		enabled: false,
+	},
+	github: {
+		enabled: true,
+	},
+	rss: {
+		enabled: false,
+	},
+};
