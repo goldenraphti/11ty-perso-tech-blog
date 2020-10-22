da2874b0899ef0f7b777f8831eb602a91f92bb70
raphaelferrand <ferrand@protonmail.ch>
2020-10-22 22:38:15 +0200

    enable ESlint & Prettier

--- _data/editorSettings.js
+++ _data/editorSettings.js
@@ -1,20 +1,26 @@
 module.exports = {
-	translations: {
-		enabled: false,
-	},
-	darkMode: {
-		enabled: false,
-	},
-	email: {
-		enabled: true,
-	},
-	twitter: {
-		enabled: false,
-	},
-	github: {
-		enabled: true,
-	},
-	rss: {
-		enabled: false,
-	},
+  emailLink: {
+    enabled: true,
+  },
+  sideProjects: {
+    enabled: false,
+  },
+  translations: {
+    enabled: false,
+  },
+  darkMode: {
+    enabled: false,
+  },
+  email: {
+    enabled: true,
+  },
+  twitter: {
+    enabled: false,
+  },
+  github: {
+    enabled: true,
+  },
+  rss: {
+    enabled: false,
+  },
 };
