561152925a6a837f4483585a470c984d43e1a092
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2022-03-16 13:32:28 +0100

    add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   // Copy '/css' to '_site/css'
@@ -11,6 +12,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
   return {
     passthroughFileCopy: true,
   };
